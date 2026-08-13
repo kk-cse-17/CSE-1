25efba17082476b18205e7686de9fed65ea21aeb
kk-cse-17 <kkarthikrao8@gmail.com>
2026-08-13 15:17:51 +0530

    Implement attendance checker logic

---
+++ attendance.py
@@ -0,0 +1,9 @@
+#ATTENDANCE CHECKER PROGRAM
+
+
+attendance=83  #We set the student's attendance percentage
+
+if attendance >=62
+ print("Attendance is sufficient! You are eligible for exams")
+else:
+    print("Warning!!!: Attendance is low. Please meet your teacher")
